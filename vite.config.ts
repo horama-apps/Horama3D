efdbcc77016cb3e45ad14af14742daffa79e77de
David Castro <davcastroruiz@gmail.com>
2026-07-01 17:57:23 -0600

    right port

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const stpApiTarget = process.env.STP_API_TARGET ?? 'http://127.0.0.1:8000';
+const stpApiTarget = process.env.STP_API_TARGET ?? 'http://127.0.0.1:1111';
 
 export default defineConfig({
   plugins: [react()],
